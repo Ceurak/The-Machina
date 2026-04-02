f0854f93478c27e1fd1a1d28a4284a986c06c821
Ceurak <o.rachcinski@proton.me>
2026-04-02 21:10:47 +0200

    Zmiana app.py na scanner.py - rozbudowa parsowania danych (złączenie ich jako jeden słownik), wypisanie ich. Dodanie pliku app.py - tu będzie wstępnie widok z TUI.

--- app.py
+++ app.py
@@ -1,40 +1,9 @@
-import subprocess
-import re
+from textual.app import App
+from textual.widgets import Static, DataTable
 
-# Regex'y potrzebne
-regex_ip = re.compile(r'\b(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\b')
-regex_mac = re.compile(r'\b(?:[0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}\b')
+class ScanApp(App):
+    def compose(self):
+        
 
-# Arp-scan - pobranie IP i MAC hostów
-results_arp = subprocess.run(
-    ["sudo", "arp-scan", "--localnet"],
-    capture_output=True,
-    text=True
-)
-
-line_arp = results_arp.stdout.splitlines()
-
-devices = []
-
-# Wyszukanie ip i mac regexem i przypisanie do zmiennej
-for line in line_arp:
-    match_ip = regex_ip.search(line)
-    match_mac = regex_mac.search(line)
-
-    if match_ip and match_mac:
-        ip = match_ip.group()
-        mac = match_mac.group()
-        devices.append((ip, mac))
-
-# Wynik arp'a
-for ip, mac in devices:
-    print(f"IP: {ip} | MAC: {mac}")
-
-range = input("Podaj zakres IP do skanu: ")
-results_nmap = subprocess.run(
-    ["nmap", range],
-    capture_output=True,
-    text=True
-)
-
-print(results_nmap.stdout)
+if __name__ == "__main__":
+    ScanApp().run()
---
+++ scanner.py
@@ -0,0 +1,64 @@
+import subprocess
+import re
+
+# Regex'y potrzebne
+regex_ip = re.compile(r'\b(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\b')
+regex_mac = re.compile(r'\b(?:[0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}\b')
+
+# Zakres działania
+range = input("Podaj zakres IP do skanu: ")
+
+# Arp-scan - pobranie IP i MAC hostów
+results_arp = subprocess.run(
+    ["sudo", "arp-scan", range],
+    capture_output=True,
+    text=True
+)
+
+line_arp = results_arp.stdout.splitlines()
+
+
+# Utworzenie słownika z samymi kluczami dla hostów (bez portów)
+devices = []
+
+# Wyszukanie ip i mac regexem i przypisanie do zmiennej
+for line in line_arp:
+    match_ip = regex_ip.search(line)
+    match_mac = regex_mac.search(line)
+
+    if match_ip and match_mac:
+        ip = match_ip.group()
+        mac = match_mac.group()
+        devices.append({
+            "Hostname": ip,
+            "IP": ip,
+            "MAC": mac
+        })
+
+
+# Nmap - pobranie IP, hostname,
+results_nmap = subprocess.run(
+    ["nmap", "-sn", "-R", range],
+    capture_output=True,
+    text=True
+)
+line_nmap = results_nmap.stdout.splitlines()
+
+for line in line_nmap:
+    line = line.strip()
+
+    match_host = re.match(r"Nmap scan report for (.+) \((\d+\.\d+\.\d+\.\d+)\)", line)
+    if match_host:
+        hostname = match_host.group(1)
+        ip = match_host.group(2)
+
+        for device in devices:
+            if device["IP"] == ip:
+                device["Hostname"] = hostname
+                break
+
+
+# Wypisanie danych
+print("\nWykryte hosty w danym zakresie. Jeśli brak wykrytego hostname - zastąpiąny jest jako IP.")
+for device in devices:
+    print(f"HOSTNAME: {device['Hostname']} | MAC: {device['MAC']} | IP: {device['IP']}")
